ec6e30ffb46275c154b74a1388d1965aff2de526
Imran Shaikh <168895222+imran071202@users.noreply.github.com>
2025-05-30 15:09:22 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,34 @@
+"use client"
+import React from 'react'
+import 'react-toastify/dist/ReactToastify.css';
+
+import Navbar from '@/components/Navbar'
+import Contain from '@/components/Contain'
+import Review from '@/components/Review'
+import About from '@/components/About';
+
+
+// import Agent from '@/app/Agent'
+
+
+
+
+
+const page = () => {
+  return (
+    <>
+
+      
+      <Contain />
+      <About />
+      <Review />
+
+
+      <div>
+
+      </div>
+    </>
+  )
+}
+
+export default page
